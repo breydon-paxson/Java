f65d1a54d8b08dd20ab207e69d306c64178693aa
Breydon Paxson <breydon.paxson@my.simpson.edu>
2022-04-22 09:17:30 -0500

    work

--- src/Week13/Homework/HmPrb4.java
+++ src/Week13/Homework/HmPrb4.java
@@ -1,6 +1,8 @@
 package Week13.Homework;
 
-import java.io.*;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HmPrb4 {
@@ -9,16 +11,24 @@ public class HmPrb4 {
         try {
             File file = new File("test.txt");
             Scanner sc = new Scanner(file);
-
+            ArrayList<String> lst = new ArrayList<>();
             while (sc.hasNext()) {
-                sc.next();
+                String line = sc.nextLine();
+                String shortLine = line.replace(" ","");
+                lst.add(line + " " + shortLine.length());
+
                 count++;
             }
-            System.out.println(count);
             sc.close();
+            PrintWriter out = new PrintWriter("test.txt");
+            for (String item : lst)
+                out.println(item);
+            out.close();
+
+            System.out.println(count);
+
         } catch (Exception e) {
             e.getStackTrace();
         }
     }
 }
-
